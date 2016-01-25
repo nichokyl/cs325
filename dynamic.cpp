00d0b9a9348e18c56103ae59807b1dc800dd52f0
Kyle Nichols <nichokyl@onid.oregonstate.edu>
2016-01-25 14:54:00 -0800

    Updated document

--- dynamic.cpp
+++ dynamic.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 
-#define N 8
-#define K 2
-#define B 2
+#define N 67
+#define K 16
+#define B 14
+#define LINE1 40
+#define LINE2 41
 
 using std::cout;
 using std::endl;
 
-int dist(int balls[], int keys[], int i, int n)
+int dist(int *lockers, int *balls, int *keys, int i, int n)
 {
 	int j, k, idx;
 	int distance, minDistance = n;
@@ -15,7 +19,7 @@ int dist(int balls[], int keys[], int i, int n)
 	
 //	cout << "in dist " << minDistance << " " << i << endl;
 	
-	// Checking second half
+	// Checking after i
 	for (j = i+1; j < n; j++)
 	{
 //		cout << "first loop i " << j << " keys is " << keys[j] << endl;
@@ -28,11 +32,12 @@ int dist(int balls[], int keys[], int i, int n)
 				minDistance = distance;
 				firstLoop = 1;
 				secondLoop = 0;
+				break;
 			}
 		}
 	}
 	
-	// Checking first half
+	// Checking before i
 	for (k = i-1; k >= 0; k--)
 	{
 //		cout << "second loop i " << k << " keys is " << keys[k] << endl;
@@ -46,41 +51,47 @@ int dist(int balls[], int keys[], int i, int n)
 //				cout << "min is now " << minDistance << endl;
 				firstLoop = 0;
 				secondLoop = 1;
+				break;
 			}
 		}
 	}
 	
 	if (firstLoop)
 	{
-		for (k = i; k < i+minDistance; k++)
+		for (k = j; k >= i; k--)
 		{
 //			cout << "opening " << k << endl;
+			lockers[j] = 1;
 			keys[k-1] = 1;
 			keys[k+1] = 1;
 		}
 	}
 	else if (secondLoop)
 	{
-		for (j = i; j > i-minDistance; j--)
+		for (j = k; j <= i; j++)
 		{
 //			cout << "opening " << j << endl;
+			lockers[j] = 1;
 			keys[j-1] = 1;
 			keys[j+1] = 1;
 		}
 	}
 	
+/*	for (int i = 0; i < N; i++)
+		cout << lockers[i] << " ";
+	cout << endl;
+*/
+	
 	return minDistance;
 }
 
-int minLockers(int balls[], int keys[], int n, int k, int b)
-{
-	int i;
-	
+int minLockers(int *lockers, int *balls, int *keys, int n, int k, int b)
+{	
 	// Initialize table
-	int *d = new int[b+1];
+	int *d = new int[n+1];
 	d[0] = 0;
 	
-	for (i = 1; i <= n; i++)
+	for (int i = 1; i <= n; i++)
 	{
 //		cout << "at " << i << " balls is " << balls[i-1] << " and keys is " << keys[i-1] << endl;
 		if (balls[i-1] == 0)	// There is no ball
@@ -92,44 +103,105 @@ int minLockers(int balls[], int keys[], int n, int k, int b)
 		{
 					
 //			cout << "There is a ball but no key\n";
-			int distance = dist(balls, keys, i-1, n);
+			int distance = dist(lockers, balls, keys, i-1, n);
 //			cout << "Distance is " << distance << endl;
-			d[i] = 1 + distance + d[i-distance];
+			d[i] = 1 + distance + d[(i-distance)];
 		}
 		else if (balls[i-1] == 1 && keys[i-1] == 1)	// There is a ball, but we have the key
 		{
-//			cout << "There is a ball and a key\n";
-			d[i] = 1 + d[i-1];
+//			cout << "There is a ball and a key at index" << i << endl;
+			if (lockers[i-1] == 1)	// Already opened
+			{
+//				cout << "Already opened\n";
+				d[i] = d[i-1];
+			}
+			else	// Not yet opened
+			{
+//				cout << "Not yet opened\n";
+				d[i] = 1 + d[i-1];
+				lockers[i-1] = 1;
+				keys[(i-1)-1] = 1;
+				keys[(i-1)+1] = 1;
+			}
 		}
 		else
 		{
 //			cout << "Something's wrong!\n";
 		}
+		
+//		cout << endl;
 	}
+/*	for (int i = 0; i <= N; i++)
+		cout << d[i] << " ";
+	cout << endl;
+*/
 	
 	return d[n];
 }
 
 int main()
 {
-	int lockers[N];
-	int keys[N];
-	int balls[N];
+	int *lockers = new int[N];
+	int *keys = new int[N];
+	int *balls = new int[N];
 	
-	int i;
-	for (i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)
+		lockers[i] = 0;
+	for (int i = 0; i < N; i++)
 		keys[i] = 0;
-	for (i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)
 		balls[i] = 0;
 	
-	keys[0] = 1;
-	keys[7] = 1;
+	// Read input
+	char line[1024];
+	int val, count = 1;
+	FILE *fp = fopen("dp.txt", "r");
+
+	for (int i = 1; i < LINE1; i++)
+		fgets(line, 1024, fp);
 	
-	balls[4] = 1;
-	balls[3] = 1;
+	for (int i = 0; i < K; i++)
+	{
+		fscanf(fp, "%d ", &val);
+//		cout << val << " ";
+		keys[val-1] = 1;
+	}
+//	cout << endl;
 	
-	int min = minLockers(balls, keys, N, K, B);
+//	cout << "done reading keys" << endl;
+	
+	fp = fopen("dp.txt", "r");
+	for (int i = 1; i < LINE2; i++)
+		fgets(line, 1024, fp);
+	
+//	cout << line << endl;
+//	cout << "now at line " << LINE2 << endl;
+	
+	for (int i = 0; i < B; i++)
+	{
+		fscanf(fp, "%d ", &val);
+//		cout << val << " ";
+		balls[val-1] = 1;
+	}
+//	cout << endl;
+	
+/*	for (int i = 0; i < N; i++)
+		cout << keys[i] << " ";
+	cout << endl;
+	
+	for (int i = 0; i < N; i++)
+		cout << balls[i] << " ";
+	cout << endl;
+*/
+
+	fclose(fp);
+	
+	int min = minLockers(lockers, balls, keys, N, K, B);
 	cout << min << endl;
+/*	for (int i = 0; i < N; i++)
+		cout << lockers[i] << " ";
+	cout << endl;
+*/
 	
 	return 0;
 }
